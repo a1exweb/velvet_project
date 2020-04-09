d9483b694bb5d382165f0b7655926069ba7ccb17
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-04-09 12:37:36 +0300

    Add function for gulp (moving folders)

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,7 +2,9 @@ module.exports = function () {
   $.gulp.task('copy', function () {
     return $.gulp.src([
       'img/**',
-      '*.html'
+      '*.html',
+      'fonts/**',
+      'fontawesome-pro-5.2.0-web/**'
     ], {
       base: '.'
     })
