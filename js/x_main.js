db8c2710a10ec0b9160f0238ba9aac13b68cae0d
Alexander Yaroslavtsev <a1exweb@mail.ru>
2020-04-10 10:44:12 +0300

    add modals

--- js/x_main.js
+++ js/x_main.js
@@ -30,3 +30,29 @@ $('[data-show="mobile-menu"').on('click', function() {
 $('[data-close="mobile-menu"').on('click', function() {
     $('.menu-mobile').removeClass('menu-mobile_active')
 });
+
+$('[data-show="shopping"]').on('click', function() {
+    $('.modal-shopping').addClass('modal-shopping_active')
+    $('.modal-shopping-overlay').addClass('modal-shopping-overlay_active')
+});
+
+$(window).on('click', function(event) {
+    if(event.target == $('.modal-shopping-overlay_active')[0]) {
+        $('.modal-shopping-overlay').removeClass('modal-shopping-overlay_active')
+        $('.modal-shopping').removeClass('modal-shopping_active')
+    }
+})
+
+$('[data-show="profile"]').on('click', function() {
+    $('.modal-profile').addClass('modal-profile_active')
+    $('.modal-profile-overlay').addClass('modal-profile-overlay_active')
+});
+
+$(window).on('click', function(event) {
+    if(event.target == $('.modal-profile-overlay_active')[0]) {
+        $('.modal-profile-overlay').removeClass('modal-profile-overlay_active')
+        $('.modal-profile').removeClass('modal-profile_active')
+    }
+})
+
+
